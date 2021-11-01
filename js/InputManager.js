ccc2dc45bfd5d77bc18decaf24e17d68d31f00e3
Jawad-Hamadani <jawad_h_h@hotmail.com>
2021-11-01 12:41:23 +0200

    design changes

--- js/InputManager.js
+++ js/InputManager.js
@@ -1,14 +1,11 @@
-window.addEventListener('keydown',KeyDown,true);
+window.addEventListener("keydown", KeyDown, true);
 
-function KeyDown(key){
-    if (key.key == 'ArrowRight')
-        Move(1);
-    if (key.key == 'ArrowLeft')
-        Move(-1);
-    if (key.key == 'ArrowUp')
-        Rotate();
-    if (key.key == 'ArrowDown'){
-        DropBlock();
-        DrawToScreen();
-    }
+function KeyDown(key) {
+  if (key.key == "ArrowRight") Move(1);
+  if (key.key == "ArrowLeft") Move(-1);
+  if (key.key == "ArrowUp") Rotate();
+  if (key.key == "ArrowDown") {
+    DropBlock();
+    DrawToScreen();
+  }
 }
